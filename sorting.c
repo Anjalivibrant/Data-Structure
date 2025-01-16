85a1ff5f29890ba8ae8e0762fc3c4a35bc34ad0d
Anjali Singh <121482522+Anjalivibrant@users.noreply.github.com>
2025-01-17 03:09:48 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,277 @@
+//merge sort
+#include<stdio.h> 
+void merge (int a[],int i,int j) 
+{ 
+    int k,b[50],mid,l,start; 
+    start=i; 
+    mid=(i+j)/2; 
+    k=mid+1; 
+    l=i; 
+    while(i<=mid && k<=j) 
+    { 
+        if(a[i]<=a[k]) 
+        b[l++] = a[i++]; 
+        else 
+        b[l++] = a[k++]; 
+    } 
+    if(i>mid) 
+        for(;k<=j;) 
+        b[l++]=a[k++]; 
+    else 
+        if(k>j) 
+            for(;i<=mid;) 
+            b[l++]=a[i++]; 
+        for(l=start;l<=j;l++) //reassigning values to the original array
+        a[l]=b[l]; 
+} 
+void mergesort(int a[],int i,int j)  
+{ 
+    int mid; 
+    if(i>=j) 
+    return; 
+    mid=(i+j)/2; 
+    mergesort(a,i,mid); 
+    mergesort(a,mid+1,j); 
+    merge(a,i,j); 
+} 
+void main() 
+{ 
+    int data[50],i,j,n; 
+    printf("Enter value of n:"); 
+    scanf("%d",&n); 
+    printf("n=%d\nEnter array elements:\n",n); 
+    for(i=0;i<n;i++) 
+        scanf("%d",&data[i]); 
+    printf("\n Numbers in array :"); 
+    for(i=0;i<n;i++) 
+    { 
+        printf("%d",data[i]);
+        printf("\n"); 
+    } 
+    mergesort(data,0,n-1); 
+    printf("\n Sorted elements are:"); 
+    for(i=0;i<n;i++) 
+    printf("%d\n",data[i]); 
+    printf("\n"); 
+}
+
+
+
+//Quick sort
+// Quick sort in C
+
+#include <stdio.h>
+
+// function to swap elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+  
+  // select the rightmost element as pivot
+  int pivot = array[high];
+  
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(array, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
+}
+
+// function to print array elements
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+// main function
+int main() {
+  int data[] = {8, 7, 2, 1, 0, 9, 6};
+  
+  int n = sizeof(data) / sizeof(data[0]);
+  
+  printf("Unsorted Array\n");
+  printArray(data, n);
+  
+  // perform quicksort on data
+  quickSort(data, 0, n - 1);
+  
+  printf("Sorted array in ascending order: \n");
+  printArray(data, n);
+}
+
+
+
+//insertion sort
+// Insertion sort in C
+
+#include <stdio.h>
+
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (j >=0 && key < array[j]) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  printf("Sorted array in ascending order:\n");
+  printArray(data, size);
+}
+
+
+//selection sort
+// Selection sort in C
+
+#include <stdio.h>
+
+// function to swap the the position of two elements
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+
+      // To sort in descending order, change > to < in this line.
+      // Select the minimum element in each loop.
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+  }
+}
+
+// function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+// driver code
+int main() {
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  printf("Sorted array in Acsending Order:\n");
+  printArray(data, size);
+}
+
+
+
+//bubble sort
+// Optimized implementation of Bubble sort
+#include <stdbool.h>
+#include <stdio.h>
+
+void swap(int* xp, int* yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n){
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == false)
+            break;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size){
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+int main(){
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
+
+
